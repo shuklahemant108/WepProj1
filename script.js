5af7fe811d3e9423d1807205f16d084c0f51a86b
shuklahemant108 <108146372+shuklahemant108@users.noreply.github.com>
2024-06-22 22:26:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello I m JS");
+
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
